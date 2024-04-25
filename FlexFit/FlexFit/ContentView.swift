2c8c291a4225e5b664aae41adc8f26522c106e35
Emmanuel Montoya <emmanuelmontoya02@gmail.com>
2024-04-25 16:42:57 -0700

    Created MainMenu skeleton (buttons don't work)

--- FlexFit/FlexFit/ContentView.swift
+++ FlexFit/FlexFit/ContentView.swift
@@ -9,13 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
+            Image(.flexfitAppiconTransparent)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .padding()
+                .background(Rectangle()
+                .foregroundColor(Color(red: 0.36, green: 0.43, blue: 0.52, opacity: 1.0)))
+                VStack {
+                    Text("Welcome to FlexFit")
+                    .foregroundColor(.white)
+                    .font(.title.bold())
+                    Button ("Calculate BMR"){  }
+                        .foregroundColor(.white)
+                        //.buttonStyle(.bordered)
+                        .controlSize(.extraLarge)
+                        .frame(width: 200, height: 50)
+                        .background(Rectangle().fill(Color(red: 0.36, green: 0.43, blue: 0.52, opacity: 1.0)))
+                    Button ("Calculate BMI"){  }
+                        .foregroundColor(.white)
+                        //.buttonStyle(.bordered)
+                        .controlSize(.extraLarge)
+                        .frame(width: 200, height: 50)
+                        .background(Rectangle().fill(Color(red: 0.36, green: 0.43, blue: 0.52, opacity: 1.0)))
+                    Button ("Look up Exercises"){  }
+                        .foregroundColor(.white)
+                        //.buttonStyle(.bordered)
+                        .controlSize(.extraLarge)
+                        .frame(width: 200, height: 50)
+                        .background(Rectangle().fill(Color(red: 0.36, green: 0.43, blue: 0.52, opacity: 1.0)))
+                    }
+            }
         .padding()
+        .frame(maxHeight: .infinity)
+        .background(Rectangle()
+            .foregroundColor(Color(red: 0.22, green: 0.22, blue: 0.22, opacity: 1.0)))
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
