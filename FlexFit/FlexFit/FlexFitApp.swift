8dde5cb08161283990d61b1bc4a1417e40f3d46b
Emmanuel Montoya <emmanuelmontoya02@gmail.com>
2024-04-25 15:24:02 -0700

    Created Launch Screen / Uploaded Base Files

---
+++ FlexFit/FlexFit/FlexFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlexFitApp.swift
+//  FlexFit
+//
+//  Created by Emmanuel Montoya on 4/22/24.
+//
+
+import SwiftUI
+
+@main
+struct FlexFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
